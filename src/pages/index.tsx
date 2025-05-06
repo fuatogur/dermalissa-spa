eed2bb3feaf481f26c508da536e31209f1c06f3d
Fuat Oğur <58439290+fuatogur@users.noreply.github.com>
2025-05-06 23:46:56 +0300

    hata düzeltmesi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,3 @@
-// @ts-ignore
-export async function getStaticProps({ locale, defaultLocale }) {
-    return {
-        redirect: {
-            destination: `/${locale === defaultLocale ? '' : locale + '/'}p/revitalizing-oil`,
-            permanent: false,
-        },
-    }
-}
-
 export default function Home() {
-    return null // Gerek yok, çünkü redirect ediyoruz
+    return "home"
 }
